f874f7bd5f796bc68712afc65e37d7139cd17c34
Afsana-Rima <55757169+Afsana-Rima@users.noreply.github.com>
2019-09-26 16:09:55 -0400

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+
+num1 = input("Enter the number1: ")
+num2 = input("Enter the number2: ")
+result1 = float(num1) + float(num2)
+result2 = float(num1) - float(num2)
+result3 = float(num1) * float(num2)
+result4 = float(num1) / float(num2)
+print(result1, result2, result3, result4)
+
+
